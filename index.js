30a029b73120f1810aa73d669075a5d17742deae
TungDS2 <sontungpytn>
2020-06-10 08:37:21 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+'use strict';
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+class ServerlessPlugin {
+  constructor (serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+
+    this.hooks = {
+      'package:createDeploymentArtifacts': this.packageDotNetLambda.bind(this),
+    };
+  }
+
+  async packageDotNetLambda () {
+    const cli = this.serverless.cli;
+    cli.log('Package lambda functions!');
+    let service = this.serverless.service;
+
+    const packages = service.getAllFunctions().reduce((dotnetPackages, funcName) => {
+      let func = service.getFunction(funcName);
+
+      if (this.funcRuntimeIsDotNet(service, func)) {
+        const lambdaPackage = func.package;
+        if (lambdaPackage && lambdaPackage.artifact && lambdaPackage.projectFolder) {
+          dotnetPackages.push(func.package);
+
+        }
+      }
+
+      return dotnetPackages;
+    }, []);
+    for (const p of packages) {
+      cli.log(`Start to pack function ${p.projectFolder}`)
+      await this.pack(p);
+    }
+    process.exit();
+  }
+
+  async pack (functionPackage) {
+    try {
+      const cli = this.serverless.cli;
+      const service = this.serverless.service;
+      const buildConfiguration = service.provider.buildConfiguration;
+      const framework = service.provider.framework;
+      const {stdout, stderr } = await exec(`dotnet lambda package --configuration ${buildConfiguration} --framework ${framework} --project-location ${functionPackage.projectFolder} --output-package ${functionPackage.artifact}`);
+      cli.log(stdout)
+      cli.log(stderr)
+    } catch (error) {
+      console.error('An error occured while restoring packages');
+      console.error(error);
+      process.exit(error.code);
+    }
+  }
+
+  funcRuntimeIsDotNet (service, func) {
+    let providerRuntime = service.provider.runtime;
+    let funcRuntime = func.runtime;
+
+    if (!providerRuntime && !funcRuntime) {
+      console.error('No runtime found at global provider or local function level, eg. dotnetcore2.1');
+    }
+
+    return (providerRuntime && providerRuntime.startsWith('dotnet') && !funcRuntime)
+      || (funcRuntime && funcRuntime.startsWith('dotnet'))
+      || (!providerRuntime && funcRuntime && funcRuntime.startsWith('dotnet'));
+  }
+}
+
+module.exports = ServerlessPlugin;
